cbdc0e35ec7f64ad0efc46154298bfb17c3bfa3d
ChihebDabebi <dabebi.chiheb@esprit.tn>
2025-08-04 20:46:07 +0100

    clients stats

--- routers/invoiceRouter.js
+++ routers/invoiceRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const invoiceController = require('../controllers/invoiceController');
 const {auth} = require('../middlewares/auth');
-router.post('/',auth, invoiceController.createInvoice);
+router.post('/', invoiceController.createInvoice);
 router.get('/',auth, invoiceController.getAllInvoices);
 router.get('/:id',auth, invoiceController.getInvoiceById);
 router.get('/client/:clientId',auth, invoiceController.getInvoicesByClientId);
